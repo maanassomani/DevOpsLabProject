b43442947eacc709db98d679e22700d206583951
Siddhant <siddhantgureja39@gmail.com>
2024-09-04 12:16:23 +0530

    Addition, Subtraction feature

--- calc.cpp
+++ calc.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 using namespace std;
+double add(double a , double b)
+{
+    return a  +b;
+}
 
+double subtract(double a, double b)
+{
+    if(a >= b)
+    {
+        return a-b;
+    }
+    else
+    {
+        return b-a;
+    }
+}
 int main(){
     return 0;
 }
